7c5265bc2f4518a878c0f2bb68786f5a1a1acbb6
puneet0191 <puneet.0191@gmail.com>
2021-09-13 10:48:34 +0530

    Initial Setup for npm

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# codeceptjs-twiliohelper
+# Twilio Helper for CodeceptJS
 Helper for using Twilio library to use Twilio Online Mobile Numbers and OTP based Authentication
+
+## Installation
+```
+npm i codeceptjs-twiliohelper --save
+```
+
+## Configuration
+```
+{
+  //...
+   helpers: {
+    TwilioHelper: {
+          require: "codeceptjs-twiliohelper",
+          accountID: process.env.TWILIO_ACCOUNT_ID,
+          authToken: process.env.TWILIO_ACCOUNT_AUTH_TOKEN,
+        },
+  //...
+}
+```
+
+## Usage
+
+After installing the node modules, inside your codeceptjs tests, use:
+```
+I.readSMS(sentAfter, "+XXXXXX");
+```
+The first parameter is a timestamp when the SMS OTP was triggered, the second parameter
+is the mobile number to which the SMS is sent, this will allow Twilio SDK to read
+Inbound SMS for this number post the timestamp.
+
+Please Note: readSMS returns only the most recent SMS post timestamp.
---
+++ index.js
@@ -0,0 +1,32 @@
+const Helper = codecept_helper;
+const twilio = require('twilio');
+
+class TwilioHelper extends Helper {
+
+    constructor(config) {
+        super(config);
+        if (!config.accountSid) {
+            throw new Error('Twilio Helper is not configured! Please provide accountSid');
+        }
+
+        if (!config.authToken) {
+            throw new Error('Twilio Helper is not configured! Please provide authToken');
+        }
+        this.accountSid = this.config.accountID;
+        this.authToken = this.config.authToken;
+    }
+
+    async readSMS(sentAfter, sentTo){
+        const client = twilio(this.accountSid, this.authToken);
+        let message = await client.messages
+            .list({
+                to: sentTo,
+                direction: 'inbound',
+                dateSentAfter: sentAfter
+            })
+            .then(messages => messages[0].body)
+            .catch(e => { console.error('Got an error:', e.code, e.message); });
+        return message;
+    }
+}
+module.exports = TwilioHelper;
